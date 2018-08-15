c26dc34250404d51ae44794424b55ea9f217ea4e
Huan Tran Van <huantv.ntq@gmail.com>
2018-08-15 17:58:23 +0700

    ver3

---
+++ src/main/java/com/login/social/providers/LineProvider.java
@@ -0,0 +1,46 @@
+package com.login.social.providers;
+
+import com.login.config.LineConfig;
+import com.login.model.LineEntityProfile;
+import com.login.model.LineEntityRepos;
+import com.login.model.UserBean;
+import com.login.util.Constant;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class LineProvider {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Autowired
+    LineConfig lineConfig;
+
+    public UserBean loginLine(String code, UserBean userBean){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+        map.add(Constant.LineConst.grantType, lineConfig.getGrantType());
+        map.add(Constant.LineConst.code, code);
+        map.add(Constant.LineConst.redirectUri, lineConfig.getCallBackUrl());
+        map.add(Constant.LineConst.clientId, lineConfig.getClientId());
+        map.add(Constant.LineConst.clientSecret, lineConfig.getClientSecret());
+        HttpEntity<MultiValueMap<String, String>> httpEntity = new HttpEntity<MultiValueMap<String, String>>(map, headers);
+        LineEntityRepos lineEntityRepos = restTemplate.exchange(Constant.LineConst.urlToken, HttpMethod.POST, httpEntity, LineEntityRepos.class).getBody();
+        headers.set(Constant.LineConst.authorization, Constant.LineConst.bearer + lineEntityRepos.getAccess_token());
+        HttpEntity<HttpHeaders> httpEntity1 = new HttpEntity<>(headers);
+        LineEntityProfile lineEntityProfile = restTemplate.exchange(Constant.LineConst.urlProfile, HttpMethod.GET, httpEntity1, LineEntityProfile.class).getBody();
+        userBean.setFirstName(lineEntityProfile.getDisplayName());
+        userBean.setImage(lineEntityProfile.getPictureUrl());
+        userBean.setAccesstoken(lineEntityRepos.getAccess_token());
+        return userBean;
+    }
+}
